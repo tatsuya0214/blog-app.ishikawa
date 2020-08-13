caf82641f530753cd84d47a322f6d4b405e82c10
石川竜也 <>
2020-08-14 04:23:07 +0900

    不要なonlyオプションを削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'articles#index'
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-  resources :articles, only: [:show, :new, :create, :edit, :update, :destroy]
-=======
-  resources :articles, only: [:show, :new, :create, :edit]
->>>>>>> parent of 02606e4... 記事を更新できるように変更
-=======
-  resources :articles, only: [:show, :new, :create, :edit, :update, :destroy]
->>>>>>> 14c10e2cea15bea18f322c91c7b3c0154836f2f7
+  
+  resources :articles
 end
