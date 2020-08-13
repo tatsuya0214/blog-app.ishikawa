6c43ea36d0d9816821e22239392358a01a65de56
石川竜也 <>
2020-08-14 03:36:42 +0900

    記事を削除できるようにした

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'articles#index'
 
-  resources :articles, only: [:show, :new, :create, :edit, :update]
+<<<<<<< HEAD
+  resources :articles, only: [:show, :new, :create, :edit, :update, :destroy]
+=======
+  resources :articles, only: [:show, :new, :create, :edit]
+>>>>>>> parent of 02606e4... 記事を更新できるように変更
 end
